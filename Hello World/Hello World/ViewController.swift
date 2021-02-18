fbc3c0fddac2226d9a4e5502b63001a3a8cf58b7
Colin Quelle <cquelle3@binghamton.edu>
2021-02-17 20:58:37 -0500

    Button text changes, text color changes, background color changes, image appears on button press. Created a back button option to go back and forth between opening screen and button press screen.

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -11,18 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var helloLabel : UILabel!
-    
     @IBOutlet var helloImage : UIImageView!
+    @IBOutlet var helloView: UIView!
+    @IBOutlet var helloButton: UIButton!
+    var secondScreen = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //initial settings for objects
+        helloLabel.text = "...hello?";
+        helloLabel.textColor = UIColor.black;
         helloImage.isHidden = true;
+        helloView.backgroundColor = UIColor.white;
+        helloButton.tintColor = UIColor.blue;
+        helloButton.setTitle("Press", for: .normal);
     }
     
     @IBAction func clickButton(_sender: UIButton){
-        helloLabel.text = "Hello World!"
-        helloImage.isHidden = false;
+        secondScreen = !secondScreen;
+        if(secondScreen == true){
+            helloLabel.text = "Hello World!"
+            helloLabel.textColor = UIColor.white;
+            helloImage.isHidden = false;
+            helloView.backgroundColor = UIColor.blue;
+            helloButton.tintColor = UIColor.white;
+            helloButton.setTitle("Back", for: .normal);
+        }
+        else{
+            helloLabel.text = "...hello?";
+            helloLabel.textColor = UIColor.black;
+            helloImage.isHidden = true;
+            helloView.backgroundColor = UIColor.white;
+            helloButton.tintColor = UIColor.blue;
+            helloButton.setTitle("Press", for: .normal);
+        }
     }
 }
 
