e8460c0a94af23ba1380588d48f55e208c999eed
Colin Quelle <cquelle3@binghamton.edu>
2021-02-19 10:40:02 -0500

    Added buttons to change background color

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet var helloImage : UIImageView!
     @IBOutlet var helloView: UIView!
     @IBOutlet var helloButton: UIButton!
+    
+    @IBOutlet var redButton: UIButton!
+    @IBOutlet var blueButton: UIButton!
+    @IBOutlet var greenButton: UIButton!
+    
     var secondScreen = false;
     
     override func viewDidLoad() {
@@ -29,6 +34,10 @@ class ViewController: UIViewController {
         helloButton.setTitle("Press", for: .normal);
         
         helloImage.layer.cornerRadius = 20;
+        
+        redButton.isHidden = true;
+        greenButton.isHidden = true;
+        blueButton.isHidden = true;
     }
     
     @IBAction func clickButton(_sender: UIButton){
@@ -40,6 +49,9 @@ class ViewController: UIViewController {
             helloView.backgroundColor = UIColor.blue;
             helloButton.tintColor = UIColor.white;
             helloButton.setTitle("Back", for: .normal);
+            redButton.isHidden = false;
+            greenButton.isHidden = false;
+            blueButton.isHidden = true;
         }
         else{
             helloLabel.text = "...hello?";
@@ -48,7 +60,31 @@ class ViewController: UIViewController {
             helloView.backgroundColor = UIColor.white;
             helloButton.tintColor = UIColor.blue;
             helloButton.setTitle("Press again?", for: .normal);
+            redButton.isHidden = true;
+            greenButton.isHidden = true;
+            blueButton.isHidden = true;
         }
+    }
+    
+    @IBAction func redBG(_sender: UIButton){
+        helloView.backgroundColor = UIColor.red;
+        blueButton.isHidden = false;
+        greenButton.isHidden = false;
+        redButton.isHidden = true;
+    }
+    
+    @IBAction func blueBG(_sender: UIButton){
+        helloView.backgroundColor = UIColor.blue;
+        redButton.isHidden = false;
+        greenButton.isHidden = false;
+        blueButton.isHidden = true;
+    }
+    
+    @IBAction func greenBG(_sender: UIButton){
+        helloView.backgroundColor = UIColor.green;
+        redButton.isHidden = false;
+        blueButton.isHidden = false;
+        greenButton.isHidden = true;
     }
 }
 
